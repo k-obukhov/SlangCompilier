b659b5bad69959af637f19a4577557b58a8d98df
= <nothingisgood@yandex.ru>
2020-01-28 02:54:09 +0400

    Basic backend infrastructure

---
+++ SLangCompilier/BackEnd/BackendCompiler.cs
@@ -0,0 +1,24 @@
+﻿using SLangCompiler.BackEnd.Translator;
+using SLangCompiler.FrontEnd.Tables;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SLangCompiler.BackEnd
+{
+    public class BackendCompiler
+    {
+        private readonly SourceCodeTable table;
+        private readonly BaseBackendTranslator translator;
+        public BackendCompiler(SourceCodeTable table, BaseBackendTranslator translator)
+        {
+            this.table = table;
+            this.translator = translator;
+        }
+
+        public void Translate()
+        {
+
+        }
+    }
+}
---
+++ SLangCompilier/BackEnd/Translator/BaseBackEndTranslator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SLangCompiler.BackEnd.Translator
+{
+    public abstract class BaseBackendTranslator
+    {
+        private readonly StringBuilder builder;
+
+        public BaseBackendTranslator()
+        {
+            builder = new StringBuilder();
+        }
+
+        public string GetText() => builder.ToString();
+    }
+}
