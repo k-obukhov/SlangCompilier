8c8a8d742b68d723cb2058a98e99f54f984c921d
= <nothingisgood@yandex.ru>
2020-03-04 22:15:02 +0400

    backend: statements

--- SLangCompilier/BackEnd/Translator/CppTranslatorComplexStatements.cs
+++ SLangCompilier/BackEnd/Translator/CppTranslatorComplexStatements.cs
@@ -1,11 +1,61 @@
 ﻿using SLangGrammar;
 using SLangCompiler.FrontEnd.Types;
 using Antlr4.Runtime.Misc;
+using System.Linq;
 
 namespace SLangCompiler.BackEnd.Translator
 {
     // if, while, do-while
     public partial class CppTranslator : SLangGrammarBaseVisitor<object>
     {
+        public override object VisitIfC([NotNull] SLangGrammarParser.IfCContext context)
+        {
+            var exps = context.exp();
+            var stmts = context.statementSeq();
+
+            for (int i = 0; i < exps.Length; ++i)
+            {
+                if (i == 0)
+                {
+                    cppText.Write("if (");
+                    Visit(exps[i]);
+                    cppText.Write(')');
+                }
+                else
+                {
+                    cppText.Write("else if(");
+                    Visit(exps[i]);
+                    cppText.Write(')');
+                }
+                Visit(stmts[i]);
+            }
+
+            if (context.Else() != null)
+            {
+                cppText.Write("else");
+                Visit(stmts.Last());
+            }
+            return null;
+        }
+
+        public override object VisitWhileC([NotNull] SLangGrammarParser.WhileCContext context)
+        {
+            cppText.Write("while (");
+            Visit(context.exp());
+            cppText.Write(")");
+            Visit(context.statementSeq());
+            return null;
+        }
+
+        public override object VisitRepeatC([NotNull] SLangGrammarParser.RepeatCContext context)
+        {
+            cppText.Write("do ");
+            Visit(context.statementSeq());
+            cppText.Write("while (");
+            Visit(context.exp());
+            cppText.Write(")");
+            
+            return null;
+        }
     }
 }
--- SLangCompilier/BackEnd/Translator/CppTranslatorSimpleStatements.cs
+++ SLangCompilier/BackEnd/Translator/CppTranslatorSimpleStatements.cs
@@ -7,10 +7,60 @@ namespace SLangCompiler.BackEnd.Translator
     // return, call, input, output, let,
     public partial class CppTranslator : SLangGrammarBaseVisitor<object>
     {
+
+        public override object VisitStatement([NotNull] SLangGrammarParser.StatementContext context)
+        {
+            VisitChildren(context);
+            cppText.WriteLine();
+            return null;
+        }
         public override object VisitSimpleStatement([NotNull] SLangGrammarParser.SimpleStatementContext context)
         {
-            base.VisitSimpleStatement(context);
+            VisitChildren(context);
             cppText.Write(';');
+            return null;
+        }
+
+        public override object VisitReturnC([NotNull] SLangGrammarParser.ReturnCContext context)
+        {
+            cppText.Write("return ");
+            Visit(context.exp());
+            return null;
+        }
+
+        public override object VisitCall([NotNull] SLangGrammarParser.CallContext context)
+        {
+            Visit(context.designator());
+            return null;
+        }
+
+        public override object VisitInput([NotNull] SLangGrammarParser.InputContext context)
+        {
+            cppText.Write("cin");
+            for (int i = 0; i < context.designator().Length; ++i)
+            {
+                cppText.Write(" >> ");
+                Visit(context.designator()[i]);
+            }
+            return null;
+        }
+
+        public override object VisitOutput([NotNull] SLangGrammarParser.OutputContext context)
+        {
+            cppText.Write("cout");
+            for (int i = 0; i < context.exp().Length; ++i)
+            {
+                cppText.Write(" << ");
+                Visit(context.exp()[i]);
+            }
+            return null;
+        }
+
+        public override object VisitLet([NotNull] SLangGrammarParser.LetContext context)
+        {
+            Visit(context.designator());
+            cppText.Write(" = ");
+            Visit(context.exp());
             return null;
         }
     }
