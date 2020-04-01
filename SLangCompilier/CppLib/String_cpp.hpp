a0c48f9fecf628ee729f7d3f05eab39b661e5223
= <nothingisgood@yandex.ru>
2020-04-02 00:45:04 +0400

    str module

---
+++ SLangCompilier/CppLib/String_cpp.hpp
@@ -0,0 +1,62 @@
+﻿#ifndef __STRING_CPP_H
+#define __STRING_CPP_H
+
+#include<string>
+
+using namespace std;
+
+namespace String_cpp
+{
+	bool str_eq_(string left, string right)
+	{
+	    return left == right;
+	}
+
+	bool str_neq_(string left, string right)
+	{
+	    return left != right;
+	}
+
+	bool str_l_(string left, string right)
+	{
+	    return left < right;
+	}
+
+	bool str_g_(string left, string right)
+	{
+	    return left > right;
+	}
+
+	bool str_leq_(string left, string right)
+	{
+	    return left <= right;
+	}
+
+	bool str_geq_(string left, string right)
+	{
+	    return left >= right;
+	}
+
+	string str_char_(string left, char right)
+	{
+		return left + right;
+	}
+
+	string str_concat_(string left, string right)
+	{
+		return left + right;
+	}
+
+	string str_itos_(int64_t value)
+	{
+		return to_string(value);
+	}
+
+	string str_rtos_(double value)
+	{
+		return to_string(value);
+	}
+
+}
+
+#endif
